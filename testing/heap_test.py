ae48ba0f1af228de82c977b40d4b791691c897f0
Daniel Jeffries <danjeffries96@gmail.com>
2018-07-21 16:45:42 -0400

    added extreme_descendants test to heap test case

--- testing/heap_test.py
+++ testing/heap_test.py
@@ -14,7 +14,7 @@ class GenericHeapTest(unittest.TestCase):
     def setUp(self):
         """Build a BST of size 50 using only inserts"""
         self.heap = tree.BinaryHeap()
-        self.size = 500
+        self.size = 10
 
         self.original_values = random.sample(list(range(self.size * 2)), self.size)
 
@@ -48,3 +48,35 @@ class GenericHeapTest(unittest.TestCase):
 
             self.assertEqual(occurrences, 1, msg=message)
 
+    def test_extreme_descendants(self):
+        """
+        Test if extreme descendants are correct for all subtrees.
+        Extreme descendants are min/max values of those
+        at deepest level of subtree.
+        """
+
+        for subtree in self.heap:
+            if subtree.is_leaf():
+                xleft = xright = subtree
+            else:
+                all_descendants = list(subtree)
+                max_d = max([node.depth for node in all_descendants])
+                only_max_depth = list(filter(lambda n: n.depth == max_d, all_descendants))
+                # xleft = min(only_max_depth, key=lambda n: n.val)
+                # xright = max(only_max_depth, key=lambda n: n.val)
+
+                xleft = only_max_depth[0]
+                xright = only_max_depth[-1]
+
+            self.assertEqual(xleft, subtree.xleft, msg="Extreme left of %s should be %s" % (subtree, xleft))
+            self.assertEqual(xright, subtree.xright, msg="Extreme right of %s should be %s" % (subtree, xright))
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+    """
+         0
+      1     2
+    3   4  5  6
+    """
