be7f3ebee300fc8abce3a3c45cf8b72989895fe1
Daniel Jeffries <danjeffries96@gmail.com>
2018-09-03 13:45:52 -0400

    refactored Array constructor to accept size or iterable as single positional argument

--- datastructures/arrays.py
+++ datastructures/arrays.py
@@ -1,4 +1,6 @@
 from datastructures.basic import DataStructure
+from drawtools.render import RenderArray
+from drawtools import dsDraw_colors
 import random
 
 
@@ -12,26 +14,27 @@ class ArrayNode(object):
 
 
 class Array(DataStructure):
-    def __init__(self, prebuild_values=None, size=0, hide=False):
+    def __init__(self, prebuild=None, hide=False):
         """
         Basic Array class composed of
         ArrayNodes. ArrayNodes currently contain
         information about value and color.
 
-        :param prebuild_values: some iterable of _array
+        :param prebuild: integer or iterable of values
         """
         super().__init__()
 
-        if prebuild_values:
-            self._array = [ArrayNode(value=x) for x in prebuild_values]
-            self.size = len(self._array)
-        elif size:
-            self.size = size
-            self._array = [ArrayNode(value=random.randint(0, self.size * 10)) for _ in range(self.size)]
-        else:
-            # 8 cells fit nicely on screen as default length
+        if prebuild is None:
+            # 8 cells fit nicely on screen as default size
             self.size = 8
-            self._array = [ArrayNode(value=random.randint(0, self.size * 10)) for _ in range(self.size)]
+            self._array = [ArrayNode(value=random.randint(0, 99)) for _ in range(self.size)]
+        elif isinstance(prebuild, int):
+            self.size = prebuild
+            self._array = [ArrayNode(value=random.randint(0, 99)) for _ in range(self.size)]
+        else:
+            # prebuild is some iterable
+            self._array = [ArrayNode(value=x) for x in prebuild]
+            self.size = len(self._array)
 
         self.hide_values = hide
 
@@ -39,11 +42,15 @@ class Array(DataStructure):
         return "Array of size %s" % self.size
 
     def __iter__(self):
-        return self._array
+        for x in self._array:
+            yield x
+
+    def __len__(self):
+        return self.size
 
     def __getitem__(self, index):
         """
-        Access _array.
+        Access _array
         """
         return self._array[index].value
 
@@ -56,11 +63,32 @@ class Array(DataStructure):
         self._array[index] = ArrayNode(value, color=cur_color)
 
     def clone(self):
-        clone = Array(prebuild_values=self._array)
+        clone = Array(prebuild=self._array)
         return clone
 
     def get_render_class(self):
-        return None
+        return RenderArray
+
+    def color(self, indices, color_name):
+        """
+        Assign a new color to array[index].
+        Must be valid color (in dsDraw_colors)
+        :param indices: index or tuple of indices
+        :param color_name: key to dsDraw_colors dict
+        """
+        try:
+            color = dsDraw_colors[color_name]
+        except KeyError:
+            raise Exception("Invalid color '%s'" % color_name)
+
+        if isinstance(indices, int):
+            index = indices
+            self._array[index].color = color
+        else:
+            low, high = indices
+            for i in range(low, high):
+                self._array[i].color = color
+
 
 if __name__ == '__main__':
     a = Array(size=10)
